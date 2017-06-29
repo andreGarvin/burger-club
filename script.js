541b6b00889ef8bd9ceb8d8a8c97c138a0d12469
Dre_ <andre.garvin@envirostudies.org>
2017-06-29 20:29:54 +0000

    update

--- script.js
+++ script.js
@@ -2,26 +2,22 @@ function not_empty( input ) {
     
     if ( input.length !== 0 ) {
         
-        return true
+        return true;
     }
     
-    return false
+    return false;
 }
 
 
 $(document).ready(function() {
     
-    $('#myModal').on('show.bs.modal', function( e ) {
+    $('#sign_up').click(function() {
         if ( not_empty( $('#email').val() ) && not_empty( $('#name').val() ) ) {
-            
-            var modal = $(this)
-            modal.find('#text').html('HI - <span style="color: gray">'+ $('#name').val() +'</span> thank you for signing up! We will send an email to <span style="color: gray">'+ $('#email').val() +'</span> shortly')
         
-            $('.form-control').val('')
-        }
-        else {
-            
-            console.log('Please provide the information needed');
+            $('#myModal').modal('show');
+            $('#myModal').find('#text').html('HI - <span style="color: gray">'+ $('#name').val() +'</span> thank you for signing up! We will send an email to <span style="color: gray">'+ $('#email').val() +'</span> shortly')
+        
+            // $('.form-control').val('')
         }
     })
 
