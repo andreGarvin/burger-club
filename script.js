1f76975069ac875dcfdf652886e321ff9581bc7a
Dre_ <andre.garvin@envirostudies.org>
2017-06-29 17:59:05 +0000

    commting the chages mae to the index.html file adding the modal and carseoal, as well as changing the positioning. as well as adding the code in the script.js file

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
-$(document).ready(function() {
-
-   
+function not_empty( input ) {
     
+    if ( input.length !== 0 ) {
+        
+        return true
+    }
+    
+    return false
+}
+
+
+$(document).ready(function() {
+    
+    $('#myModal').on('show.bs.modal', function( e ) {
+        if ( not_empty( $('#email').val() ) && not_empty( $('#name').val() ) ) {
+            
+            var modal = $(this)
+            modal.find('#text').html('HI - <span style="color: gray">'+ $('#name').val() +'</span> thank you for signing up! We will send an email to <span style="color: gray">'+ $('#email').val() +'</span> shortly')
+        
+            $('.form-control').val('')
+        }
+        else {
+            
+            console.log('Please provide the information needed');
+        }
+    })
+
+    $(".social_media").hover(function() {
+        $("style").css("background-color", "blue");
+    })
+
+
+    $('.carousel').carousel({
+        interval: 2000
+        
+    })
 
 });
